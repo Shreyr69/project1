fe6ac900b0e82d4f6ec55c8c7fd45431805a7427
Shreyr69 <shreyrai69@gmail.com>
2026-01-29 16:57:53 +0530

    added search function

--- script.js
+++ script.js
@@ -24,3 +24,37 @@ async function fetchProducts() {
 }
 
 fetchProducts();
+
+
+async function searchProducts() {
+    const query = document.getElementById("searchInput").value;
+
+    if (!query) return;
+
+    try {
+        const response = await fetch(
+            `https://dummyjson.com/products/search?q=${query}`
+        );
+        const data = await response.json();
+
+        const container = document.getElementById("searchResults");
+        container.innerHTML = "";
+
+        data.products.forEach(product => {
+            const card = document.createElement("div");
+            card.className = "product-card";
+
+            card.innerHTML = `
+                <img src="${product.thumbnail}" alt="${product.title}">
+                <div class="product-title">${product.title}</div>
+                <div class="price">₹ ${product.price}</div>
+            `;
+
+            container.appendChild(card);
+        });
+
+    } catch (error) {
+        console.log("Error searching products:", error);
+    }
+}
+
